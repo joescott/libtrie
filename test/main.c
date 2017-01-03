7e38c63483f12d729d862aa3d5e68f849ef8dd40
Jorge <jorge.courett@gmail.com>
2017-01-02 23:38:42 -0300

    changes

--- test/main.c
+++ test/main.c
@@ -9,6 +9,10 @@ int main(int argc, char **argv)
     trieAdd(&trie,"test",NULL);
     trieAdd(&trie,"testa",NULL);
     trieAdd(&trie,"fest",NULL);
+    trieSearch(trie,"testa");
+    trieRemove(&trie,"testa");
+    trieSearch(trie,"testa");
+    trieDestroy(trie);
 	return EXIT_SUCCESS;
 }
 
--- trie.c
+++ trie.c
@@ -28,16 +28,9 @@ TRIE_NODE_ST *trieCreate(void)
     return root;
 }
 
-void trieDestroy(TRIE_NODE_ST  *root)
-{
-    free(root);
-}
-
 TRIE_NODE_ST* trieSearch(TRIE_NODE_ST *root, const TRIE_NODE_KEY_T *key)
 {
 	TRIE_NODE_ST *level = root;
-	TRIE_NODE_ST *pPtr = NULL;
-
 
 	int lvl=0;
 	while(1)
@@ -45,23 +38,23 @@ TRIE_NODE_ST* trieSearch(TRIE_NODE_ST *root, const TRIE_NODE_KEY_T *key)
 		TRIE_NODE_ST *found = NULL;
 		TRIE_NODE_ST *curr;
 
-		for (curr = level; curr != NULL; curr = curr->next)
-		{
-			if (curr->key == *key)
+		for(curr = level; curr != NULL; curr = curr->next)
+			if(curr->key == *key)
 			{
 				found = curr;
 				lvl++;
 				break;
 			}
-		}
 
-		if (found == NULL)
+		if(found == NULL)
 			return NULL;
+	
+        printf("Search [%c]\n",*key);
 
-		if (*key == '\0')
+		if(*key == '\0')
 		{
-			pPtr = curr;
-			return pPtr;
+		    printf("Found! at level %d\n",lvl);
+			return curr;
 		}
 
 		level = found->children;
@@ -71,78 +64,194 @@ TRIE_NODE_ST* trieSearch(TRIE_NODE_ST *root, const TRIE_NODE_KEY_T *key)
 
 void trieAdd(TRIE_NODE_ST **root, TRIE_NODE_KEY_T *key, TRIE_NODE_VALUE_T value)
 {
-	TRIE_NODE_ST *node;
-	if(*root == NULL)
-		return;
-	node = (*root)->children;
+    TRIE_NODE_ST *node;
+    if(*root == NULL)
+        return;
 
-	if(node == NULL) /** First Node */
-	{
-		for(node = *root;; node = node->children, key++)
-		{
-			node->children = trieNodeNew(node, *key);
-			printf("Inserting: [%c]\n",node->children->key);
-			if(*key == '\0')
-				break;
-		}
-		return;
-	}
+    node = (*root)->children;
 
-	if(trieSearch(node, key))
-	{
-		printf("Duplicate!\n");
-		return;
-	}
+    /** If First Node */
+    if(node == NULL) 
+    {
+        for(node = *root;; node = node->children, key++)
+        {
+            node->children = trieNodeNew(node, *key);
+            printf("Inserting: [%c]\n",node->children->key);
+            if(*key == '\0')
+                break;
+        }
+        return;
+    }
+
+    /** If Duplicate */
+    if(trieSearch(node, key))
+    {
+        printf("Duplicate!\n");
+        return;
+    }
 
 
-	while(*key != '\0')
-	{
-		if(*key == node->key)
-		{
-			key++;
-			printf("Traversing child: [%c]\n",node->children->key);
-			node = node->children;
-		}
-		else
-			break;
-	}
+    while(*key != '\0')
+    {
+        if(*key == node->key)
+        {
+            key++;
+            printf("Traversing child: [%c]\n",node->children->key);
+            node = node->children;
+        }
+        else
+            break;
+    }
 
-	while(node->next)
-	{
-		if(*key == node->next->key)
-		{
-			key++;
-			trieAdd(&(node->next), key, value);
-			return;
-		}
-		node = node->next;
-	}
+    while(node->next)
+    {
+        if(*key == node->next->key)
+        {
+            key++;
+            trieAdd(&(node->next), key, value);
+            return;
+        }
+        node = node->next;
+    }
 
-	if(*key == '\0')
-	{
-		node->next = trieNodeNew(node->parent, *key);
-	}
-	else
-	{
-		node->next = trieNodeNew(node->parent, *key);
-	}
+    if(*key == '\0')
+    {
+        node->next = trieNodeNew(node->parent, *key);
+        printf("A\n");
+    }
+    else
+    {
+        node->next = trieNodeNew(node->parent, *key);
+        printf("B\n");
+    }
 
-	node->next->prev = node;
-	printf("Inserting [%c] as neighbour of [%c] \n",node->next->key, node->key);
+    node->next->prev = node;
+    printf("Inserting [%c] as neighbour of [%c] \n",node->next->key, node->key);
 
-	if(!(*key))
-		return;
+    if(*key == '\0')
+        return;
 
-	key++;
+    key++;
 
-	for(node = node->next; ; node = node->children, key++)
-	{
-		node->children = trieNodeNew(node, *key);
-		printf("Inserting: [%c]\n",node->children->key);
-		if(*key == '\0')
-			break;
-		node->children = trieNodeNew(node, *key);
-	}
+    for(node = node->next; ; node = node->children, key++)
+    {
+        node->children = trieNodeNew(node, *key);
+        printf("Inserting: [%c]\n",node->children->key);
+        if(*key == '\0')
+            break;
+        node->children = trieNodeNew(node, *key);
+    }
 
-	return;
+    return;
+}
+
+
+void trieRemove(TRIE_NODE_ST **root, TRIE_NODE_KEY_T *key)
+{
+    TRIE_NODE_ST *tPtr = NULL;
+    TRIE_NODE_ST *tmp = NULL;
+
+    if(NULL == *root || NULL == key)
+        return;
+
+    tPtr = trieSearch((*root)->children, key);
+
+    if(NULL == tPtr)
+    {
+        printf("Key [%s] not found in trie\n", key);
+        return;
+    }
+
+    printf("Deleting key [%s] from trie\n", key);
+
+    while(1)
+    {
+        if( tPtr->prev && tPtr->next)
+        {
+            tmp = tPtr;
+            tPtr->next->prev = tPtr->prev;
+            tPtr->prev->next = tPtr->next;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+            break;
+        }
+        else if(tPtr->prev && !(tPtr->next))
+        {
+            tmp = tPtr;
+            tPtr->prev->next = NULL;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+            break;
+        }
+        else if(!(tPtr->prev) && tPtr->next)
+        {
+            tmp = tPtr;
+            tPtr->parent->children = tPtr->next;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+            break;
+        }
+        else
+        {
+            tmp = tPtr;
+            tPtr = tPtr->parent;
+            tPtr->children = NULL;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+        }
+    }
+
+    printf("Deleted key [%s] from trie\n", key);
+}
+ 
+ 
+void trieDestroy(TRIE_NODE_ST  *root)
+{
+    TRIE_NODE_ST *tPtr = root;
+    TRIE_NODE_ST *tmp = root;
+
+    while(tPtr)
+    {
+        while(tPtr->children)
+            tPtr = tPtr->children;
+
+        if( tPtr->prev && tPtr->next)
+        {
+            tmp = tPtr;
+            tPtr->next->prev = tPtr->prev;
+            tPtr->prev->next = tPtr->next;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+        }
+        else if(tPtr->prev && !(tPtr->next))
+        {
+            tmp = tPtr;
+            tPtr->prev->next = NULL;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+        }
+        else if(!(tPtr->prev) && tPtr->next)
+        {
+            tmp = tPtr;
+            tPtr->parent->children = tPtr->next;
+            tPtr->next->prev = NULL;
+            tPtr = tPtr->next;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+        }
+        else
+        {
+            tmp = tPtr;
+            if(tPtr->parent == NULL)
+            {
+                /*Root*/
+                free(tmp);
+                return;
+            }
+            tPtr = tPtr->parent;
+            tPtr->children = NULL;
+            printf("Deleted [%c] \n", tmp->key);
+            free(tmp);
+        }
+    }
 }
